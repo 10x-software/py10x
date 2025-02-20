dc7fabdc0be8c8c3e69fd016b817339ca85342f3
AMD <78048725+sasha-davidovich@users.noreply.github.com>
2025-02-20 15:42:05 +0400

    cp - save is functional!

---
+++ core_10x/manual_tests/ts_trivial_test.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    from core_10x.code_samples.person import Person
+    from infra_10x.mongodb_store import MongoStore
+
+
+    db = MongoStore.new_instance('localhost', 'test', '', '')
+    with db:
+        #print(db.collection_names())
+        p = Person(first_name='Sasha', last_name='Davidovich')
+        p.save()
+
+
+
+
