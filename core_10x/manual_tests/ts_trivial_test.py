7a720f0140182605ff6b0e18c5ad35a4ef43177e
IAP <27013475+ilya-pevzner@users.noreply.github.com>
2025-02-21 18:11:05 -0500

    minor test cleanup

--- core_10x/manual_tests/ts_trivial_test.py
+++ core_10x/manual_tests/ts_trivial_test.py
@@ -1,6 +1,7 @@
 if __name__ == '__main__':
     from core_10x.code_samples.person import Person
     from infra_10x.mongodb_store import MongoStore
+    from core_10x import BCache
 
     to_save = True
 
@@ -9,6 +10,7 @@ if __name__ == '__main__':
     with db:
         #print(db.collection_names())
 
+        #Person.collection().delete('Sasha|Davidovich')
         if to_save:
             p = Person(first_name = 'Sasha', last_name = 'Davidovich')
             rev = p.save()
