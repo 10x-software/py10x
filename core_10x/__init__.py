e8bb0ebeaf43c9e1f3709dee65d703b5408c32df
ilya-pevzner <pevzner@nyu.edu>
2025-02-21 18:15:57 -0500

    # release references to python objects so they can be deleted before the interpreter exits

--- core_10x/__init__.py
+++ core_10x/__init__.py
@@ -1,7 +1,15 @@
-from core_10x_i import PyLinkage
+from core_10x_i import PyLinkage, BCache
 from core_10x.xnone import XNone
 from core_10x.rc import RC_TRUE
 from core_10x.trait import TraitMethodError
 
 PyLinkage.init(XNone, RC_TRUE, TraitMethodError)
 PyLinkage.redirect_stdout_to_python()
+
+class _Finalizer:
+    def __del__(self):
+        # release references to python objects that have to be deleted before the interpreter exits
+        BCache.clear()
+        PyLinkage.clear()
+
+_finalizer = _Finalizer()
