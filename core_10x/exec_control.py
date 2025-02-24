faa8ddf4552bc4546bebbc8a62c1b0fe411df10b
AMD <78048725+sasha-davidovich@users.noreply.github.com>
2025-02-24 14:37:10 +0400

    cp: exec_control.py - after moving most if it to c++, things started to work a bit

--- core_10x/exec_control.py
+++ core_10x/exec_control.py
@@ -1,34 +1,18 @@
-from core_10x_i import BTraitableProcessor
-from core_10x_i import SimpleCacheLayer
+from core_10x_i import BTraitableProcessor as BTP
 
-class XControl:
-    @staticmethod
-    def new_xcontrol(graph: bool, debug: bool, convert_values: bool) -> 'XControl':
-        flags = 0x0
-        if graph:
-            flags |= BTraitableProcessor.ON_GRAPH
-        if debug:
-            flags |= BTraitableProcessor.DEBUG
-        elif convert_values:
-            flags |= BTraitableProcessor.CONVERT_VALUES
-
-        return XControl(BTraitableProcessor.create(flags), graph)
-
-    def __init__(self, btp: BTraitableProcessor, graph: bool):
-        self.btp = btp
-        if graph:
-            cache = SimpleCacheLayer()
-            self.btp.use_own_cache(cache)
-
-    def __enter__(self):
-        self.btp.begin_using()
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.btp.end_using()
 
 def GRAPH_ON(debug = False, convert_values = False):
-    return XControl.new_xcontrol(True, debug, convert_values)
+    flags = BTP.ON_GRAPH
+    if debug:
+        flags |= BTP.DEBUG
+    elif convert_values:
+        flags |= BTP.CONVERT_VALUES
+    return BTP.create(flags)
 
 def GRAPH_OFF(debug = False, convert_values = False):
-    return XControl.new_xcontrol(False, debug, convert_values)
+    flags = 0
+    if debug:
+        flags |= BTP.DEBUG
+    elif convert_values:
+        flags |= BTP.CONVERT_VALUES
+    return BTP.create(flags)
