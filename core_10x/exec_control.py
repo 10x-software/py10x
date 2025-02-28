a3581b716613228d6ef0803ae8ba45e6027abff8
AMD <78048725+sasha-davidovich@users.noreply.github.com>
2025-02-28 17:20:38 +0400

    cp: exec_control.py - use the new BTP.create(int, int, int)

--- core_10x/exec_control.py
+++ core_10x/exec_control.py
@@ -1,13 +1,10 @@
-from functools import partial
+import functools
 
 from core_10x_i import BTraitableProcessor as BTP
 
-def _create(flags, debug=None, convert_values=None):
-    if debug or debug is None and BTP.current().flags() & BTP.DEBUG:
-        flags |= BTP.DEBUG
-    elif convert_values or convert_values is None and BTP.current().flags() & BTP.CONVERT_VALUES:
-        flags |= BTP.CONVERT_VALUES
-    return BTP.create(flags)
 
-GRAPH_ON = partial(_create, BTP.ON_GRAPH)
-GRAPH_OFF= partial(_create, 0)
+def GRAPH_ON(debug: bool = -1, convert_values: bool = -1):
+    return BTP.create(1, convert_values, debug)
+
+def GRAPH_OFF(debug: bool = -1, convert_values: bool = -1):
+    return BTP.create(0, convert_values, debug)
