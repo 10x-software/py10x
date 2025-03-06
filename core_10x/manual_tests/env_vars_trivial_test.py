5d3b6bf8f74bf34b942bda10e0c574b5bc9713ba
AMD <78048725+sasha-davidovich@users.noreply.github.com>
2025-03-06 16:15:40 +0400

    changed the implementation of EnvVars NOT to use Traitables - via cached class properties

--- core_10x/manual_tests/env_vars_trivial_test.py
+++ core_10x/manual_tests/env_vars_trivial_test.py
@@ -1,21 +1,14 @@
-from datetime import date
-
-from core_10x.env_vars import EnvVars, XDateTime, M
-
-class CompanyEnvVars(EnvVars, env_name = 'YYY_LLC'):
-    date_format     = M(XDateTime.FORMAT_US)
 
 if __name__ == '__main__':
+    from datetime import date
+    from core_10x.environment_variables import EnvVars, XDateTime
+
     d = date.today()
     print(XDateTime.date_to_str(d))
 
-    #ev = EnvVars()
-
-    df_10x = EnvVars.X.date_format
+    df_10x = EnvVars.date_format
     print(XDateTime.date_to_str(d))
 
-    df_yyy = EnvVars.YYY_LLC.date_format
-    print(XDateTime.date_to_str(d))
 
 
 
