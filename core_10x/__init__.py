0d0b6d359ccfc8a1963930a25d5bb5121c92a8fd
AMD <alexander.m.davidovich@gmail.com>
2025-03-07 23:54:49 +0400

    BCache -> XCache

--- core_10x/__init__.py
+++ core_10x/__init__.py
@@ -1,4 +1,4 @@
-from core_10x_i import PyLinkage, BCache
+from core_10x_i import PyLinkage, XCache
 from core_10x.xnone import XNone
 from core_10x.rc import RC_TRUE
 from core_10x.trait import TraitMethodError
@@ -9,7 +9,7 @@ PyLinkage.redirect_stdout_to_python()
 class _Finalizer:
     def __del__(self):
         # release references to python objects that have to be deleted before the interpreter exits
-        BCache.clear()
+        XCache.clear()
         PyLinkage.clear()
 
 _finalizer = _Finalizer()
