54acb2805a1b7788f5b882a3cf63b57b41511588
AMD <alexander.m.davidovich@gmail.com>
2025-03-08 10:30:46 +0400

    1) exec_control.py: + INTERACTIVE() 2) traitable_trivial_test.py: check that newly created obj with the existing ID "inherits" other trait values

--- ui_10x/examples/entity_stocker.py
+++ ui_10x/examples/entity_stocker.py
@@ -1,4 +1,5 @@
 if __name__ == '__main__':
+    from core_10x.exec_control import INTERACTIVE
     from core_10x.code_samples.person import Person
 
     from infra_10x.mongodb_store import MongoStore
@@ -9,13 +10,11 @@ if __name__ == '__main__':
     ux.init()
 
     with MongoStore.instance(hostname = 'localhost', dbname = 'test', username = '', password = ''):
-        plug = StockerPlug()
-        plug.current_class = Person
+        plug = StockerPlug(current_class = Person)
+        se = EntityStocker(plug = plug)
 
-        se = EntityStocker()
-        se.plug = plug
-
-        w = ux.Widget()
-        w.set_layout(se.main_layout())
-        d = UxDialog(w)
-        rc = d.exec()
+        with INTERACTIVE() as graph:
+            w = ux.Widget()
+            w.set_layout(se.main_layout())
+            d = UxDialog(w)
+            rc = d.exec()
