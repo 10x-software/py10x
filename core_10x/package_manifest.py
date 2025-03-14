8c39d466e9f37c65f0282fd210afd6d0a94b759a
AMD <78048725+sasha-davidovich@users.noreply.github.com>
2025-03-14 07:30:55 +0300

    cp: package_manifest.py - comments and more defaults

--- core_10x/package_manifest.py
+++ core_10x/package_manifest.py
@@ -1,7 +1,44 @@
 from core_10x.py_class import PyClass
 from core_10x.resource import ResourceRequirements
+from core_10x.data_domain import GeneralDomain
 
 class PackageManifest:
+    """
+    To associate a subclass of Traitable to a particular Data Domain Category
+    it must have a record in a package manifest file.
+    Example:
+        # module x.etrading_domain
+        class ETradingDomain(DataDomain):
+            GENERAL     = TS_STORE()
+            ORDERS      = TS_STORE()
+            ALGO_ORDERS = TS_STORE()
+            ...
+
+        #module x.y.order
+        class Order(Traitable):
+            ...
+
+        class AlgoOrder(Order):
+            ...
+
+
+        #module x.y._package_manifest
+        from x.etrading_domain import ETradingDomain as ET
+
+        manifest = dict(        #-- variable manifest: dict must be defined as follows
+            _category = ET.GENERAL,     #-- default category for Traitables in x.y package, if any
+
+            order = dict(                   #-- specific association(s) for any module inside x.y package, if any
+                _category = ET.ORDERS           #-- default category for Traitables in x.y.order, if any
+
+                AlgoOrder = ET.ALGO_ORDERS      #-- specific association class x.y.order.AlgoOrder, if any
+                ...
+            ),
+            ...
+        )
+
+        If no association could be found for a subclass of Traitable, the default association will be used: GeneralDomain.GENERAL
+    """
     SYMBOL      = '_package_manifest.manifest'
     CATEGORY    = '_category'
 
@@ -19,9 +56,15 @@ class PackageManifest:
         module_entry = man_def.get(short_module)
         if module_entry:
             rr: ResourceRequirements = module_entry.get(cls.__name__)
-            if rr:
-                return rr
+            if not rr:
+                rr = module_entry.get(PackageManifest.CATEGORY)
+                if rr:
+                    return rr
 
-        return man_def.get(PackageManifest.CATEGORY)
+        rr = man_def.get(PackageManifest.CATEGORY)
+        if rr:
+            return rr
+
+        return GeneralDomain.GENERAL
 
     #-- TODO: handle parent classes, if ResourceRequirements for the class in not found
