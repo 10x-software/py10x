de3cf10429f41f3ac4059759f84d1ec32209dca6
AMD <78048725+sasha-davidovich@users.noreply.github.com>
2025-04-03 14:33:18 +0400

    cp: package names (modules, classes and methods) are passed to PyLinkage

--- core_10x/__init__.py
+++ core_10x/__init__.py
@@ -1,11 +1,37 @@
-from os import path
+# from os import path
+#
+# pp = path.dirname(path.abspath(__file__))
+# path_to_core_10x = path.dirname(pp)
 
-pp = path.dirname(path.abspath(__file__))
-path_to_core_10x = path.dirname(pp)
+from core_10x_i import CORE_10X, PyLinkage, XCache
 
-from core_10x_i import PyLinkage, XCache
 
-PyLinkage.init(path_to_core_10x)
+PyLinkage.init({
+    CORE_10X.PACKAGE_NAME:                  __name__,
+
+    CORE_10X.XNONE_MODULE_NAME:                 'xnone',
+    CORE_10X.XNONE_CLASS_NAME:                      'XNone',
+
+    CORE_10X.RC_MODULE_NAME:                    'rc',
+    CORE_10X.RC_TRUE_NAME:                          'RC_TRUE',
+
+    CORE_10X.NUCLEUS_MODULE_NAME:               'nucleus',
+    CORE_10X.NUCLEUS_CLASS_NAME:                    'Nucleus',
+
+    CORE_10X.ANONYMOUS_MODULE_NAME:             'traitable',
+    CORE_10X.ANONYMOUS_CLASS_NAME:                  'AnonymousTraitable',
+
+    CORE_10X.TRAITABLE_ID_MODULE_NAME:          'traitable_id',
+    CORE_10X.TRAITABLE_ID_CLASS_NAME:               'ID',
+
+    CORE_10X.TRAIT_METHOD_ERROR_MODULE_NAME:    'trait_method_error',
+    CORE_10X.TRAIT_METHOD_ERROR_CLASS_NAME:         'TraitMethodError',
+
+    CORE_10X.PACKAGE_REFACTORING_MODULE_NAME:   'package_refactoring',
+    CORE_10X.PACKAGE_REFACTORING_CLASS_NAME:        'PackageRefactoring',
+    CORE_10X.PACKAGE_REFACTORING_FIND_CLASS:            'find_class',
+    CORE_10X.PACKAGE_REFACTORING_FIND_CLASS_ID:         'find_class_id',
+})
 
 class _Finalizer:
     def __del__(self):
