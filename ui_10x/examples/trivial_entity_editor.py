6942c13ba5cc7cc83bdb484ff964a16b5f70f50e
IAP <27013475+ilya-pevzner@users.noreply.github.com>
2025-09-17 20:20:44 -0400

    fix manual test

--- ui_10x/examples/trivial_entity_editor.py
+++ ui_10x/examples/trivial_entity_editor.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
         #weight_qu   = UiMod(flags = Ui.HIDDEN),
     )
     e = TraitableEditor.editor(p, view = view)
-    rc = e.popup(save=True)
+    rc = e.dialog(save=True).exec()
 
 
 
