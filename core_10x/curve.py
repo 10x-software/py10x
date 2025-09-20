bfe5d570053435af5d4099850e29928ae63dc991
AMD <78048725+sasha-davidovich@users.noreply.github.com>
2025-09-20 07:23:56 +0400

    cp 1

---
+++ core_10x/curve.py
@@ -0,0 +1,301 @@
+import bisect
+import math
+from datetime import date, timedelta
+from scipy import interpolate
+from typing import Any
+
+from core_10x.traitable import Traitable, T, RT, RC, RC_TRUE
+from core_10x.named_constant import NamedConstant
+
+class IP_KIND(NamedConstant, lowercase_values = True):
+    #-- NOTE: enum labels specify the minimum number of points required!
+
+    LINEAR      = ( 2, )
+    NEAREST     = ( 2, )
+    NEAREST_UP  = ( 2, )
+    PREVIOUS    = ( 2, )    #-- return the previous value of the point
+    NEXT        = ( 2, )    #-- ------ the next ----
+    ZERO        = ( 1, )    #-- spline interp of zeroth order
+    SLINEAR     = ( 2, )    #-- spline interp of first order
+    QUADRATIC   = ( 3, )    #-- spline interp of second order
+    CUBIC       = ( 4, )    #-- spline interp of third order
+
+    NO_INTERP   = ( 0, )    #-- NO interp outside of given nodes
+
+    # @classmethod
+    # def labelFromValue( cls, value ) -> int:
+    #     return int( super().labelFromValue( value ) )
+    #
+    # @classmethod
+    # def defaultValueForName( cls, name: str ) -> str:
+    #     return name.replace( '_', '-' ).lower()
+
+class CurveParams(Traitable):
+    DEFAULT_INTERPOLATOR = interpolate.interp1d
+
+    interpolator: Any   = RT(default = DEFAULT_INTERPOLATOR)
+    ip_kind: IP_KIND    = T(IP_KIND.LINEAR)
+    assume_sorted: bool = T(True)
+    copy: bool          = T(False)
+    fill_value: str     = T('extrapolate')
+    bounds_error: bool  = T(False)
+
+class Curve(Traitable):
+    times: list         = T([])       #-- only ints or floats are allowed
+    values: list        = T([])
+    params: CurveParams = T(T.EMBEDDED)
+
+    beginning_of_time: Any  = T(None)    #-- may be float or int
+
+    interpolator: Any       = RT()
+    min_curve_size: int     = RT()
+
+    def params_get(self) -> CurveParams:
+        return CurveParams()
+
+    def min_curve_size_get(self) -> int:
+        return self.params.ip_kind.label
+
+    def start_time(self):
+        times = self.times
+        return times[0] if times else None
+
+    def end_time(self):
+        times = self.times
+        return times[-1] if times else None
+
+    def update(self, t, value, reset = True):
+        times = self.times
+        values = self.values
+        i = bisect.bisect_left(times, t)
+        if i >= len(times):
+            times.append(t)
+            values.append(value)
+        else:
+            if times[i] == t:
+                values[i] = value
+            else:
+                times.insert(i, t)
+                values.insert(i, value)
+
+        self.times = times
+        self.values = values
+        if reset:
+            self.reset()
+
+    def update_many(self, times, values, reset = True):
+        assert len(times) == len(values), 'times and values size mismatch'
+        for i, t in enumerate(times):
+            self.update(t, values[i], reset = False)
+
+        if reset:
+            self.reset()
+
+    def remove(self, t, reset = True) -> bool:
+        times = self.times
+        if t not in times:
+            return False
+
+        values = self.values
+        i = times.index(t)
+        times.pop(i)
+        values.pop(i)
+        if reset:
+            self.reset()
+
+        return True
+
+    def perturb(self, t, new_value, perturb_existing_only = False):
+        if perturb_existing_only:
+            assert t in self.times, f't = {t} is not in the curve'
+
+        values = self.values
+        self.invalidate_value('values')
+        self.values = values
+        self.update(t, new_value)
+
+    def perturb_shift(self, t, value_shift, perturb_existing_only = False):
+        self.perturb(t, self.value(t) + value_shift, perturb_existing_only)
+
+    def perturb_proportional(self, t, value_shift_mult, perturb_existing_only = False):
+        self.perturb(t, self.value(t) * value_shift_mult, perturb_existing_only)
+
+    def interpolator_get(self):
+        params = self.params
+        return params.interpolator(
+            self.times, self.values,
+            kind            = params.ip_kind,
+            assume_sorted   = params.assume_sorted,
+            copy            = params.copy,
+            fill_value      = params.fill_value,
+            bounds_error    = params.bounds_error,
+        )
+
+    def value(self, t) -> float:
+        times = self.times
+
+        if self.params.ip_kind is IP_KIND.NO_INTERP:
+            return self.values[times.index(t)] if t in times else math.nan
+
+        if len(times) < self.min_curve_size:
+            if t in times:
+                return self.values[times.index(t)]
+
+        bot = self.beginning_of_time
+        return float(self.interpolator(t)) if (not bot) or (t >= bot) else math.nan
+
+    def values_at(self, dates) -> tuple:
+        return tuple(self.value(d) for d in dates)
+
+    def reset(self):
+        self.invalidate_value('interpolator')
+
+    @classmethod
+    def _uniqueTimesValues( cls, times: list, values: list, keep_last_update: bool ) -> tuple:
+        assert len( times ) == len( values ), f'{len(times)} != {len( values)}: sizes of times and values must be equal'
+
+        last_t = times[ 0 ]
+        last_i = 0
+        times_unique = [ last_t ]
+        values_unique = [ values[ 0 ] ]
+
+        for t, v in zip( times, values ):
+            assert t >= last_t, f'times must be a non-decreasing list, but {t} < {last_t}'
+
+            if t > last_t:
+                last_t = t
+                last_i += 1
+                times_unique.append( t )
+                values_unique.append( v )
+            else:
+                if keep_last_update:
+                    values_unique[ last_i ] = v
+
+        return ( times_unique, values_unique )
+
+    def uniquePointsCurve( self, keep_last_update = True, copy_curve = False ) -> 'Curve':
+        times_unique, values_unique = self._uniqueTimesValues( self.times(), self.values(), keep_last_update )
+        if copy_curve:
+            return self.clone( times = times_unique, values = values_unique )
+
+        self.times = times_unique
+        self.values = values_unique
+        self.reset()
+        return self
+
+class TwoFuncInterpolator:
+    def __init__(self, in_func, out_func, in_func_on_arrays = None, _interpolator = interpolate.interp1d):
+        if in_func:
+            in_func_on_arrays = lambda list_x, list_y: [ in_func(x, list_y[i]) for i, x in enumerate(list_x) ]
+
+        assert in_func_on_arrays, 'either in_func or in_func_on_arrays must be provided'
+
+        self.in_func = in_func_on_arrays
+        self.out_func = out_func
+        self.interpolator = _interpolator
+
+    def __call__(self, x, y, **kwargs):
+        values = self.in_func(x, y)
+        ip_method = self.interpolator(x, values, **kwargs)
+        out_func = self.out_func
+
+        def _method(t):
+            v = ip_method(t)
+            return out_func(t, v)
+
+        return _method
+
+class DateCurve(Curve):
+    dates: list = RT()
+
+    s_epoch_date = date(1970, 1, 1)
+
+    @classmethod
+    def _to_number(cls, d) -> int:
+        if isinstance(d, date):
+            return (d - cls.s_epoch_date).days
+        elif isinstance(d, int):
+            return d
+
+        assert False, f'Unexpected value {d}'
+
+    @classmethod
+    def _from_number(cls, x: int) -> date:
+        return cls.s_epoch_date + timedelta(days = x)
+
+    def dates_get( self ) -> list:
+        f = self._from_number
+        return [ f(x) for x in self.times ]
+
+    def dates_set(self, trait, value) -> RC:
+        f = self._to_number
+        times = [ f(d) for d in value ]
+        return self.set_value('times', times)
+
+    def beginning_of_time_set(self, trait, d) -> RC:
+        t = self._to_number(d)
+        self.raw_set_value(trait, t)
+        return RC_TRUE
+
+    def start_time(self) -> date:
+        times = self.dates
+        return times[0] if times else None
+
+    def end_time(self) -> date:
+        times = self.dates
+        return times[-1] if times else None
+
+    def update(self, d: date, value, reset = True):
+        x = self._to_number(d)
+        super().update(x, value, reset = reset)
+
+    def reset(self):
+        super().reset()
+        self.invalidate_value('dates')
+
+    def value(self, d: date) -> float:
+        x = self._to_number(d)
+        return super().value(x)
+
+    def remove(self, d, reset = True):
+        t = self._to_number(d)
+        if super().remove(t, reset = reset):
+            self.invalidate_value('dates')
+
+    def perturb(self, d: date, new_value, perturb_existing_only = False):
+        if perturb_existing_only:
+            assert d in self.dates, f'{d } is not in the curve'
+
+        values = self.values
+        self.invalidate_value('values')
+        self.values = values
+        self.update(d, new_value)
+
+    def bracketDateNodes( self, d: date ) -> tuple:
+        t = self._toNumber( d )
+
+        times: list = self.times()
+        last_time = times[ -1 ]
+        first_time = times[ 0 ]
+        if t > last_time:
+            return ( self._fromNumber( last_time ), None )
+
+        if t < first_time:
+            return ( None, self._fromNumber( first_time ) )
+
+        i = bisect.bisect_left( times, t )
+        if times[ i ] == t:
+            d = self._fromNumber( times[ i ] )
+            return ( d, d )
+
+        d_left = self._fromNumber( times[ i - 1 ] )
+        d_right = self._fromNumber( times[ i ] )
+        return ( d_left, d_right )
+
+    def beginningOfTimeAsDate( self ) -> date:
+        return DateCurve._fromNumber( self.beginning_of_time() )
+
+    def datesValues( self, min_date: date = None, max_date: date = None ) -> list:
+        dates = self.dates()
+        values = self.values()
+        return [ ( d, v ) for d, v in zip( dates, values ) if ( not min_date or d >= min_date ) and ( not max_date or d <= max_date ) ]
---
+++ ui_10x/examples/guess_word.py
@@ -0,0 +1,298 @@
+from core_10x.entity import Entity, RT, T, Ui, RC, RC_TRUE
+import core_10x.trait_definition as trait_definition
+
+from ui_10x.utils import ux
+from ui_10x.platform_interface import Widget, VBoxLayout, HBoxLayout, PushButton
+from ui_10x.table_view import TableView
+
+class CHAR_STATE:
+    NONE        = ( 'lightgray',    'black' )
+    WRONG_POS   = ( 'white',        'black' )
+    BINGO       = ( 'darkgreen',    'white' )
+
+
+class GuessResult( Entity ):
+    the_word: str    = RT()
+    guess: str       = RT()
+    num_chars: int   = RT()
+
+    demo_class: type = RT()
+
+    char_state: list = RT()
+
+    guessed_chars: list = RT()
+    current_char: int   = RT(0)
+
+    def guessed_chars_get(self) -> list:
+        return list(self.num_chars * ' ')
+
+    def set_current_char(self, c: str):
+        i = self.current_char
+        guessed_chars = self.guessed_chars
+        guessed_chars[i] = c
+        self.current_char = i + 1
+
+    def delete_char(self):
+        i = self.current_char
+        if i:
+            i -= 1
+            guessed_chars = self.guessed_chars
+            guessed_chars[i] = ' '
+            self.current_char = i
+
+    def accept_guess(self):
+        self.guess = ''.join(self.guessed_chars)
+
+    def num_chars_get(self) -> int:
+        return len(self.the_word)
+
+    def char_state_get(self) -> list:
+        num_chars = self.num_chars
+        res = [ CHAR_STATE.NONE ] * num_chars
+        word = list(self.the_word)
+        for i, c in enumerate(self.guess):
+            try:
+                pos = word.index(c)
+                res[i] = CHAR_STATE.BINGO if pos == i else CHAR_STATE.WRONG_POS
+                word[pos] = ''
+            except ValueError:
+                pass
+
+        return res
+
+    def guess_get(self) -> str:
+        return self.num_chars * ' '
+
+    @classmethod
+    def _create_char_trait(cls, i: int):
+        name = f'char_{i}'
+        getter_name = f'{name}_get'
+        sh_name = f'{name}_style_sheet'
+        getter = lambda self: self.guess[i]
+        getter.__name__ = getter_name
+        sh_getter = lambda self: T.colors(*self.char_state[i])
+        sh_getter.__name__ = sh_name
+        setattr(cls, getter_name, getter)
+        setattr(cls, sh_name, sh_getter)
+        #return ( name, T( str, f'{i+1}' ) )
+        return (
+            name,
+            trait_definition.TraitDefinition(
+                **{
+                    trait_definition.NAME_TAG:      name,
+                    trait_definition.DATATYPE_TAG:  str,
+                    trait_definition.FLAGS_TAG:     T.RUNTIME
+                }
+            )
+        )
+
+    def keyboard(self):
+        w = Widget()
+        lay = VBoxLayout()
+        w.set_layout(lay)
+
+        for i, keys in enumerate(self.s_keys):
+            l = HBoxLayout()
+            for j, c in enumerate(keys):
+                b = PushButton(c)
+                styles = self.s_style.get(i)
+                if styles:
+                    sh = styles.get(j)
+                    if sh:
+                        b.set_style_sheet(sh)
+
+                avg_char_width = w.font_metrics().average_char_width()
+                b.setMaximumWidth(3 * avg_char_width)
+
+                l.add_widget(b)
+            lay.add_layout(l)
+
+        return w
+
+class Key:
+    def __init__(self, guess: GuessResult, c: str, stylesheet: str, callback):
+        self.guess = guess
+        self.c = c
+        self.stylesheet = stylesheet
+        self.callback = callback
+        self.w: PushButton = None
+
+    def widget(self, parent_w: Widget ) -> PushButton:
+        if not self.w:
+            self.w = b = PushButton(self.c)
+            b.clicked_connect(lambda w: self.callback(w))
+            b.set_style_sheet(self.stylesheet)
+            avg_char_width = parent_w.font_metrics().average_char_width()
+            b.setMaximumWidth(3 * avg_char_width)
+
+        return self.w
+
+class Keyboard:
+    s_keys = [
+        'qwertyuiop',
+        'asdfghjkl',
+        '*zxcvbnm<'
+    ]
+    s_specialChars = {
+        2:  {
+            0:                      ( 'background-color: green; color: white',  GuessResult.accept_guess ),
+            len(s_keys[2]) - 1:     ( 'background-color: red; color: white',    GuessResult.delete_char ),
+        }
+    }
+
+    def __init__(self, guess: GuessResult):
+        self.guess = guess
+        special_chars = self.s_specialChars
+        num_rows = len(self.s_keys)
+        self.keys = keys = [ [] ] * num_rows
+        for i, symbols in enumerate(self.s_keys):
+            for j, c in enumerate( symbols ):
+                sh = ''
+                cb = GuessResult.set_current_char
+                exc = special_chars.get(i)
+                if exc:
+                    spec = exc.get(j)
+                    if spec:
+                        sh, cb = spec
+
+                key = Key(guess, c, sh, cb)
+                keys[i].append(key)
+
+    def widget( self ) -> Widget:
+        ...
+
+class Game(Entity):
+    the_word: str   = RT()
+    num_chars: int  = RT()
+    guess: str      = RT( ui_hint = Ui('>'))
+    push: bool      = RT( ui_hint = Ui('*', widget_type = Ui.WIDGET_TYPE.PUSH, right_label = True, max_width = 3))
+
+    count: int      = RT(6)
+    current: int    = RT(0)
+
+    guess_res_class: type   = RT()
+    attempts: list          = RT()
+    table: TableView        = RT()
+
+    def the_word_set(self, trait, value: str) -> RC:
+        self.raw_set_value(trait, value.upper())
+        return RC_TRUE
+
+    def guess_set(self, trait, value: str) -> RC:
+        self.raw_set_value(trait, value.upper())
+        return RC_TRUE
+
+    def num_chars_get(self) -> int:
+        return len(self.the_word)
+
+    def guess_res_class_get(self):
+        num_chars = self.num_chars
+        class Demo(GuessResult):
+            @classmethod
+            def ownTraitDirSource( cls ) -> dict:
+                return { tri[ 0 ]: tri[ 1 ] for i in range( num_chars ) if ( tri := cls._create_char_trait( i ) ) }
+
+        Demo.traits()
+        return Demo
+
+    def guess_result(self, guess: str):
+        demo_class = self.guess_res_class
+        return demo_class(the_word = self.the_word, guess = guess)
+
+    def push_get(self) -> bool:
+        return self.current < self.count
+
+    def attempts_get(self) -> list:
+        num_chars = self.num_chars
+        return [ self.guess_result(num_chars * ' ') for _ in range(self.count) ]
+
+    def push_action( self, editor ):
+        current = self.current()
+        guess = self.guess()
+        if len( guess ) != self.num_chars():
+            uxWarning( f'The word is {self.num_chars()} characters long.' )
+            return
+
+        gr = self.guess_result( self.guess() )
+        attempts = self.attempts()
+        attempts[ current ] = gr
+        self.table().renderEntity( current, None )
+
+        if all( s == CHAR_STATE.BINGO for s in gr.char_state() ):
+            uxSuccess( 'Great job - you got it!')
+
+        current += 1
+        if current >= self.count():
+            uxWarning( 'Unfortunately you have failed this time' )
+
+        self.current = current
+
+
+    def table_get( self ) -> TableView:
+        table = TableView( self.attempts() )
+        hv = table.horizontalHeader()
+        hv.setStretchLastSection( False )
+
+        return table
+
+    s_keys = [
+        'qwertyuiop',
+        'asdfghjkl',
+        '*zxcvbnm<'
+    ]
+    s_style = {
+        2:  {
+            0:                          'background-color: green; color: white',
+            len( s_keys[ 2 ] ) - 1:     'background-color: red; color: white'
+        }
+    }
+    def keyboard( self ):
+        w = QWidget()
+        lay = QVBoxLayout()
+        w.setLayout( lay )
+
+        for i, keys in enumerate( self.s_keys ):
+            l = QHBoxLayout()
+            for j, c in enumerate( keys ):
+                b = QPushButton( c )
+                styles = self.s_style.get( i )
+                if styles:
+                    sh = styles.get( j )
+                    if sh:
+                        b.setStyleSheet( sh )
+
+                avg_char_width = w.fontMetrics().averageCharWidth()
+                b.setMaximumWidth( 3 * avg_char_width )
+
+                l.addWidget( b )
+            lay.addLayout( l )
+
+        return w
+
+    def widget( self ):
+        uxInit()
+        w = QWidget()
+        lay = QVBoxLayout()
+        w.setLayout( lay )
+
+        self.m_top_editor = top_editor = EntityEditor( self )
+        top = top_editor.row()
+        lay.addLayout( top )
+        lay.addWidget( uxSeparator() )
+
+        table = self.table()
+        lay.addWidget( table )
+
+        lay.addWidget( uxSeparator() )
+        lay.addWidget( self.keyboard() )
+
+        return w
+
+if __name__ == '__main__':
+    from asu.ui.entity_editor import EntityEditor
+
+    game = Game( the_word = 'credo' )
+    w = game.widget()
+
+    d = UxDialog( w, title = f'You have {game.count()} attempts to guess a word' )
+    d.exec_()
