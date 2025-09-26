176c7538b0d9b201d1316d80192b3d8a9b2a57a9
IAP <27013475+ilya-pevzner@users.noreply.github.com>
2025-09-26 19:57:21 -0400

    run headless browser even when collecting coverage

---
+++ ui_10x/rio/conftest.py
@@ -0,0 +1,23 @@
+import os
+
+import pytest
+import rio.testing.browser_client
+
+
+def running_with_coverage(config):
+    if not config.pluginmanager.getplugin('pytest_cov'):
+        return False
+
+    if not config.getoption('--cov', default='COV_CORE_SOURCE' in os.environ):
+        return False
+
+    return not config.getoption('--no-cov', default=False)
+
+@pytest.fixture(scope="session", autouse=True)
+async def manage_server(request):
+    if running_with_coverage(request.config):
+        # run headless client even if running with coverage
+        rio.testing.browser_client.DEBUGGER_ACTIVE = False
+
+    async with rio.testing.browser_client.prepare_browser_client():
+        yield
