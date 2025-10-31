67faf32a770106c6b25e7eafbe6166390ed348ac
iap <pevzner@nyu.edu>
2025-10-31 14:07:44 -0400

    add collection_name() to TsCollection interface; refactor tests

--- core_10x/unit_tests/test_ts_store.py
+++ core_10x/unit_tests/test_ts_store.py
@@ -1,12 +1,1 @@
-import pytest
 from core_10x.testlib.ts_tests import TestTSStore, ts_setup  # noqa: F401
-from core_10x.ts_store import TsStore
-
-
-@pytest.fixture(scope='module')
-def ts_instance():
-    from core_10x.testlib.test_store import TestStore
-
-    assert not TsStore.s_instances
-    yield TestStore.instance()
-    TsStore.s_instances.clear()
