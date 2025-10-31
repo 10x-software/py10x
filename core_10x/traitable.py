c839aa82426fa8225daaba15bcfb2c39d50e7363
iap <pevzner@nyu.edu>
2025-10-31 13:14:00 -0400

    ruff format

--- core_10x/traitable.py
+++ core_10x/traitable.py
@@ -281,7 +281,7 @@ class Traitable(BTraitable, Nucleus, metaclass=TraitableMetaclass):
         self.T = TraitAccessor(self)
 
     @classmethod
-    def existing_instance(cls, _collection_name: str = None, _throw = True, **trait_values) -> Traitable:
+    def existing_instance(cls, _collection_name: str = None, _throw=True, **trait_values) -> Traitable:
         obj = cls(_collection_name=_collection_name, _skip_init=True)
         if not obj.object_exists(trait_values):
             if _throw:
@@ -291,7 +291,7 @@ class Traitable(BTraitable, Nucleus, metaclass=TraitableMetaclass):
         return obj
 
     @classmethod
-    def existing_instance_by_id(cls, _id: ID = None, _id_value: str = None, _collection_name: str = None, _throw = True) -> Traitable:
+    def existing_instance_by_id(cls, _id: ID = None, _id_value: str = None, _collection_name: str = None, _throw=True) -> Traitable:
         if _id is None:
             _id = ID(_id_value, _collection_name)
         obj = cls(_id=_id)
