47bd0da2da3c1f40ebedf9308476094512d5a30f
iap <pevzner@nyu.edu>
2025-11-12 00:07:26 -0500

    cleanup mongo utils (checkpoint)

--- infra_10x/mongodb_utils.py
+++ infra_10x/mongodb_utils.py
@@ -6,141 +6,124 @@ from getpass import getpass
 
 import keyring
 import requests
+from core_10x.backbone.namespace import AUTO_PASSWORD_LENGTH, USER_ROLE
 from core_10x.global_cache import cache
 from core_10x.rc import RC
-from core_10x.ts_store import TsCollection, TsDuplicateKeyError, TsStore
 
+from infra_10x.mongodb_admin import MongodbAdmin
 from infra_10x.mongodb_store import MongoStore
 
 
 @cache
-def create_xx_role(store):
-    db = store.client.admin
-    if db.command('rolesInfo', 'xxUser')['roles']:
-        db.command('dropRole', 'xxUser')
-    db.command(
-        'createRole',
+def create_xx_role(admin: MongodbAdmin):
+    admin.db.command(
+        'updateRole' if admin.role_exists('xxUser') else 'createRole',
         'xxUser',
         privileges=[
             {
                 'resource': {'anyResource': True},
-                'actions': [
-                    'find',
-                    'insert',
-                    'update',
-                    'remove',  # read/write
-                    'createIndex',
-                    'createCollection',  # index + collection create
-                    'dropCollection',
-                ],
+                'actions': USER_ROLE.WORKER.value,
             }
         ],
         roles=[],
     )
 
 
-def drop_xx_user(store: MongoStore, username: str) -> None:
-    db = store.client['admin']
-    if db.command('usersInfo', username)['users']:
-        db.command('dropUser', username)
+def create_xx_user(admin: MongodbAdmin, xx_user: str, new_password: str) -> None:
+    password = keyring.get_password('xxuser', xx_user) or new_password
+    admin.update_user(xx_user, password, 'xxUser', keep_current_roles=False)
+    if password == new_password:
+        keyring.set_password('xxuser', xx_user, password)
 
 
-def create_xx_user(store: MongoStore, username: str, password: str) -> None:
-    create_xx_role(store)
-    drop_xx_user(store, username)
-    store.client['admin'].command('createUser', username, pwd=password, roles=['xxUser'])
-    keyring.set_password('xxuser', username, password)
-
-
-def create_xx_admin(store: MongoStore, username: str, password: str) -> None:
-    username = f'{username}-admin'
-    if username == store.auth_user():
+def create_xx_admin(admin: MongodbAdmin, xx_admin: str, new_password: str) -> None:
+    if xx_admin == admin.username:
         raise ValueError('cannot create self!')
-    drop_xx_user(store, username)
-    store.client['admin'].command('createUser', username, pwd=password, roles=['userAdminAnyDatabase', 'userAdminAnyDatabase'])
-    keyring.set_password('xxadmin', username, password)
+    password = keyring.get_password('xxadmin', xx_admin) or new_password
+    admin.update_user(xx_admin, password, 'userAdminAnyDatabase', 'readWriteAnyDatabase')
+    if password == new_password:
+        keyring.set_password('xxadmin', xx_admin, password)
 
 
-def copy_collection(from_coll: TsCollection, to_coll: TsCollection, overwrite=False) -> RC:
-    rc = RC(True)
-    for doc in from_coll.find():
-        try:
-            if not to_coll.save_new(doc, overwrite=overwrite):
-                rc.add_error(f'Failed to save {doc.get[to_coll.s_id_tag]}')
-        except TsDuplicateKeyError:
-            if overwrite:
-                raise  # -- we do not expect an exception in case of overwrite, so raise
-    return rc
-
-
-def copy_store(from_store: TsStore, to_store: TsStore, overwrite=False) -> RC:
-    rc = RC(True)
-    for collection_name in from_store.collection_names():
-        from_coll = from_store.collection(collection_name)
-        to_coll = to_store.collection(collection_name)
-        rc += copy_collection(from_coll, to_coll, overwrite=overwrite)
-    return rc
-
-
-def infer_username():
-    return requests.get('https://api.github.com/user', headers={'Authorization': f'token {get_git_token()}'}).json()['login']
+def get_xx_admin(user: str):
+    return f'{user}-admin'
 
 
 @cache
-def get_git_token():
-    return subprocess.getoutput("git credential fill <<< 'protocol=https\nhost=github.com\n' | grep password | cut -d= -f2")
+def git_user():
+    return requests.get('https://api.github.com/user', headers={'Authorization': f'token {git_token()}'}).json()['login'] or input('Username:')
 
 
 @cache
-def get_git_org():
-    return re.search(r'github\.com[/:]([\w\-\.]+)/', subprocess.check_output(['git', 'remote', 'get-url', 'origin'], text=True).strip()).group(1)
+def git_token():
+    return subprocess.getoutput("git credential fill <<< 'protocol=https\nhost=github.com\n' | grep password | cut -d= -f2") or input('Token:')
 
 
 @cache
 def all_git_users():
+    git_org = re.search(r'github\.com[/:]([\w\-\.]+)/', subprocess.check_output(['git', 'remote', 'get-url', 'origin'], text=True).strip()).group(1)
     return [
         user['login']
-        for user in requests.get(f'https://api.github.com/orgs/{get_git_org()}/members', headers={'Authorization': f'token {get_git_token()}'}).json()
+        for user in requests.get(f'https://api.github.com/orgs/{git_org}/members', headers={'Authorization': f'token {git_token()}'}).json()
     ]
+
+
+def generate_password():
+    return ''.join(secrets.choice(string.ascii_letters + string.digits + '!@#$%^&*') for _ in range(AUTO_PASSWORD_LENGTH))
+
+
+def create_users(hostname):
+    admin = MongodbAdmin(hostname=hostname, **get_credentials('xxadmin', hostname))
+    create_xx_role(admin)
+    for xx_user in all_git_users():
+        xx_admin = get_xx_admin(xx_user)
+        if xx_admin != admin.username:  # -- do not break current working account!
+            create_xx_admin(admin, xx_admin, generate_password())
+        create_xx_user(admin, xx_user, generate_password())
+
+
+def clear_vault():
+    for xx_user in all_git_users():
+        if keyring.get_password('xxuser', xx_user):
+            keyring.delete_password('xxuser', xx_user)
+        if xx_user != git_user():  # -- do not break own admin account!
+            xx_admin = get_xx_admin(xx_user)
+            if keyring.get_password('xxadmin', xx_admin):
+                keyring.delete_password('xxadmin', xx_admin)
+
+
+def drop_users(hostname):
+    admin = MongodbAdmin(hostname=hostname, **get_credentials('xxadmin', hostname))
+    for xx_user in all_git_users():
+        xx_admin = get_xx_admin(xx_user)
+        if xx_admin != admin.username:  # -- do not break current working account!
+            admin.delete_user(xx_admin)
+        admin.delete_user(xx_user)
 
 
 def get_credentials(service, hostname):
     if hostname == 'localhost':
-        return '', ''
-    username = infer_username() or input('Username:')
+        return dict(username='', password='')
+    username = git_user()
     if service == 'xxadmin':
-        username = f'{username}-admin'
+        username = get_xx_admin(username)
     password = keyring.get_password(service, username)
     if not password:
         password = getpass(f"{username}'s password for `{service}`:")
         keyring.set_password(service, username, password)
-    return username, password
+    return dict(username=username, password=password)
 
 
-def tsstore(hostname: str, dbname: str, store_cls: type[TsStore]) -> TsStore:
-    username, password = get_credentials('xxadmin' if dbname == 'admin' else 'xxuser', hostname)
-    return store_cls.instance(hostname=hostname, dbname=dbname, username=username, password=password)
-
-
-def copy_db(from_host: str, to_host: str, dbname: str, store_cls: type[TsStore], overwrite=False) -> RC:
-    return copy_store(tsstore(from_host, dbname, store_cls), tsstore(to_host, dbname, store_cls), overwrite=overwrite)
-
-
-def new_password():
-    return ''.join(secrets.choice(string.ascii_letters + string.digits + '!@#$%^&*') for _ in range(32))
-
-
-def create_users():
-    store: MongoStore = tsstore(host, 'admin', MongoStore)
-    for user in all_git_users():
-        drop_xx_user(store, f'{user}_admin')
-        if user != infer_username():
-            create_xx_admin(store, user, new_password())
-        create_xx_user(store, user, new_password())
+def copy_db(from_host: str, to_host: str, dbname: str, overwrite=False) -> RC:
+    from_store = MongoStore.instance(hostname=from_host, dbname=dbname, **get_credentials('xxuser', from_host))
+    to_store = MongoStore.instance(hostname=to_host, dbname=dbname, **get_credentials('xxuser', to_host))
+    return from_store.copy_to(to_store, overwrite=overwrite)
 
 
 if __name__ == '__main__':
     host = 'mongo10x.eastus2.cloudapp.azure.com'
-    # create_users()
+    # drop_users(host)
+    # clear_vault()
+    # create_users(host)
 
-    copy_db(from_host='localhost', to_host=host, dbname='mkt_data', store_cls=MongoStore, overwrite=True).throw()
+    copy_db(from_host='localhost', to_host=host, dbname='mkt_data', overwrite=False).throw()
