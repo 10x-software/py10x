4c74e8302b4f54819bf3b38010678e696353b75a
AMD <78048725+sasha-davidovich@users.noreply.github.com>
2026-01-28 07:04:15 +0400

    cp for merge

--- core_10x/manual_tests/ts_class_association_test.py
+++ core_10x/manual_tests/ts_class_association_test.py
@@ -1,18 +1,21 @@
 from core_10x.traitable import Traitable, T, TsClassAssociation, NamedTsStore, TsStore
 from core_10x.code_samples.person import Person
 
+class Dummy(Traitable):
+    text: str = T()
+
 class Dummy1(Traitable):
     text: str   = T()
 
-class Dummy2(Traitable):
-    text: str   = T()
+class Dummy2(Dummy):
+    ...
 
 if __name__ == '__main__':
-    from core_10x.manual_tests.ts_class_association_test import Dummy1, Dummy2
+    from core_10x.manual_tests.ts_class_association_test import Dummy, Dummy1, Dummy2
     from core_10x.py_class import PyClass
 
     extra_uris = dict(
-        dummy1 = 'mongodb://localhost/dummy1',
+        dummy = 'mongodb://localhost/dummy',
         dummy2 = 'mongodb://localhost/dummy2',
     )
     for name, uri in extra_uris.items():
@@ -24,12 +27,12 @@ if __name__ == '__main__':
         ns.save().throw()
 
     TsClassAssociation(_force = True,
-        py_canonical_name   = PyClass.name(Dummy1),
-        ts_logical_name     = 'dummy1',
+        py_canonical_name   = PyClass.name(Dummy),
+        ts_logical_name     = 'dummy',
     ).save().throw()
     TsClassAssociation(_force = True,
-        py_canonical_name   = PyClass.name(Dummy2),
-        ts_logical_name     = 'dummy2',
+        py_canonical_name   = PyClass.name(Dummy1),
+        ts_logical_name     = 'dummy1',
     ).save().throw()
 
     d1 = Dummy1(text = 'hello dummy1')
