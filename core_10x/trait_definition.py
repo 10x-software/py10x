b2201dcd52cb877e136c181388b70a441c76c629
AMD <78048725+sasha-davidovich@users.noreply.github.com>
2026-01-31 06:28:37 +0400

    minor changes across the board

--- core_10x/trait_definition.py
+++ core_10x/trait_definition.py
@@ -131,7 +131,7 @@ class TraitModification(TraitDefinition):
 
     s_modifiers = {
         FLAGS_TAG:      lambda self, flags_modification: self.flags_change(flags_modification),
-        UI_HINT_TAG:    lambda self, ui_hint_modification: ui_hint_modification.apply(self)
+        UI_HINT_TAG:    lambda self, ui_hint_modification: ui_hint_modification.apply(self.ui_hint)
     }
     # fmt: on
     def apply(self, trait_def: TraitDefinition) -> TraitDefinition:
