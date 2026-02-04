89d841d5a9c21e9e1e150fb1de12455d1bca62b9
AMD <78048725+sasha-davidovich@users.noreply.github.com>
2026-02-04 12:28:39 +0400

    cp: cm_forward.py et al - not tested yet

--- core_10x/apps/runme_1st_app.py
+++ core_10x/apps/runme_1st_app.py
@@ -1,5 +1,8 @@
 from core_10x.environment_variables import EnvVars
-from core_10x.ts_store import TsStore
+
+from core_10x.ts_store import TsStore, ResourceSpec
+from core_10x.traitable import NamedTsStore, TsClassAssociation
+
 
 def main(username: str = '', password: str = ''):
     v_main_uri: EnvVars.Var = EnvVars.var.main_ts_store_uri
@@ -27,19 +30,47 @@ Main Traitable Store is not specified, thus you must explicitly instantiate and 
 
     print(
 f"""
-1) Main Traitable Store is specified (Environment variable {EnvVars.var_name(v_main_uri.attr_name)} = {v_main_uri.value})
+Main Traitable Store is specified (Environment variable {EnvVars.var_name(v_main_uri.attr_name)} = {v_main_uri.value})
 It means that Traitable objects would be sought and saved in the above store.  
 """
     )
+
+    main_spec = TsStore.spec_from_uri(v_main_uri.value)
+    is_running, with_auth = main_spec.resource_class.is_running_with_auth(main_spec.hostname())
+    if not is_running:
+        print(f'Main Traitable Store {v_main_uri.value} is not running')
+        return
+
+    if with_auth:
+        print(f"Main Traitable Store {v_main_uri.value} requires authentication. Let's check if a Vault is specified." )
+
+        v_vault_uri = EnvVars.var.vault_ts_store_uri
+        if not v_vault_uri:
+            print(f"No Vault is specified (Environment variable {EnvVars.var_name(v_vault_uri.attr_name)} is not defined). Exiting...")
+            return
+
+        print(
+f"""
+Vault Store is specified (Environment variable {EnvVars.var_name(v_vault_uri.attr_name)} = {v_vault_uri.value})
+It means that some Traitable Stores in use (as well as other resources) require authentication.
+In order to use the Vault "automagically", you need to run "add_vault utiliry" entering your password to the Vault and your XX MasterPassword.
+"""
+        )
+        return
+
+    main_store = main_spec.resource_class.instance(**main_spec.kwargs)
     v_class_assoc = EnvVars.var.use_ts_store_per_class
     if v_class_assoc:
         print(
 f"""
-2) Different Traitable Stores may be used for different subclasses of Traitable (Environment variable {EnvVars.var_name(v_class_assoc.attr_name)} = {v_class_assoc.value})
+Different Traitable Stores may be used for different subclasses of Traitable (Environment variable {EnvVars.var_name(v_class_assoc.attr_name)} = {v_class_assoc.value})
 Checking further...
 """
         )
 
+        main_store.begin_using()
+
+        available_stores = NamedTsStore.load_many()
 
 
 if __name__ == '__main__':
