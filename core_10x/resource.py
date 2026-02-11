03b11600f2a4a1637b071fa279b0e7a67c8c6130
AMD <78048725+sasha-davidovich@users.noreply.github.com>
2026-02-11 16:59:29 +0400

    cp - mistakenly lost a method during last merge

--- core_10x/resource.py
+++ core_10x/resource.py
@@ -91,6 +91,9 @@ class ResourceSpec:
         self.resource_class = resource_class
         self.kwargs = kwargs
 
+    def hostname(self) -> str:
+        return self.kwargs[Resource.HOSTNAME_TAG]
+
     def set_credentials(self, username: str = None, password: str = None):
         if username is not None:
             self.kwargs[self.resource_class.USERNAME_TAG] = username
