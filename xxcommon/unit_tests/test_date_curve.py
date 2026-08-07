5eeec6e2b086f4d3049c3894a18a6769338d1184
IAP <27013475+ilya-pevzner@users.noreply.github.com>
2026-08-07 17:07:56 -0400

    parameterize new curve test, remove rio root from project structure, update comments

--- xxcommon/unit_tests/test_date_curve.py
+++ xxcommon/unit_tests/test_date_curve.py
@@ -1,35 +1,28 @@
-import unittest
 from datetime import date
 
-class TestDateCurve(unittest.TestCase):
-    def setUp(self):
+import pytest
+
+from xxcommon.cxx_curve import DateCurve as CxxDateCurve
+from xxcommon.py_curve import DateCurve as PyDateCurve
+
+
+class TestDateCurve:
+    def setup_method(self):
         self.d1 = date(2019, 12, 31)
         self.d2 = date(2020, 12, 31)
         self.r1 = 0.1
         self.r2 = 0.2
 
-        self.dates  = [self.d1, self.d2]
+        self.dates = [self.d1, self.d2]
         self.values = [self.r1, self.r2]
 
         self.d05 = self.d1 + (self.d2 - self.d1) / 2
         self.r05 = (self.r1 + self.r2) / 2
 
-    def test_py_curve_dates(self):
-        from xxcommon.py_curve import DateCurve
-        dc = DateCurve(dates = self.dates, values = self.values)
-        for d, r in zip(self.dates, self.values):
-            self.assertAlmostEqual(dc.value(d), r)  # add assertion here
+    @pytest.mark.parametrize('curve_class', [PyDateCurve, CxxDateCurve], ids=['py', 'cxx'])
+    def test_py_curve_dates(self, curve_class):
+        dc = curve_class(dates=self.dates, values=self.values)
+        for d, r in zip(self.dates, self.values, strict=True):
+            assert dc.value(d) == pytest.approx(r)  # add assertion here
 
-        self.assertAlmostEqual(dc.value(self.d05), self.r05)  # add assertion here
-
-    def test_cxx_curve_dates(self):
-        from xxcommon.cxx_curve import DateCurve
-        dc = DateCurve(dates=self.dates, values=self.values)
-        for d, r in zip(self.dates, self.values):
-            self.assertAlmostEqual(dc.value(d), r)  # add assertion here
-
-        self.assertAlmostEqual(dc.value(self.d05), self.r05)  # add assertion here
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert dc.value(self.d05) == pytest.approx(self.r05)  # add assertion here
