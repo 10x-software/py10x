7bf8a105564c6e28289458f7d15c023f10398e46
AAL <232821867+alex-lesin@users.noreply.github.com>
2026-08-15 01:34:28 -0400

    cp

--- xx_fin/xxfin/unit_tests/test_fin_instrument.py
+++ xx_fin/xxfin/unit_tests/test_fin_instrument.py
@@ -164,8 +164,6 @@ class TestFinInstrument:
         mkt_fx_deps_cases = [
             ((self.usd_cf_6m, self.usd_cf_6y), (('GBP', self.gbp_fx_spot, self.gbp_fx_fwds),)),
             ((self.gbp_cf_2y, self.gbp_cf_3y,), (('GBP', self.gbp_fx_spot, self.gbp_fx_fwds), ('CAD', self.cad_fx_spot, self.cad_fx_fwds))),   ## GBP/CAD resolves into GBP/USD and USD/CAD dollar crosses
-            # ((self.gbp_cf_2y, self.gbp_cf, self.gbp_cf_3y), (('GBP', self.gbp_fx_spot, self.gbp_fx_fwds), ('CAD', self.cad_fx_spot, self.cad_fx_fwds))),   ## GBP/CAD resolves into GBP/USD and USD/CAD dollar crosses
-            # ((self.gbp_cf_3y,), (('GBP', self.gbp_fx_spot, self.gbp_fx_fwds), ('CAD', self.cad_fx_spot, self.cad_fx_fwds))),   ## GBP/CAD resolves into GBP/USD and USD/CAD dollar crosses
         ]
 
         for mdc in mkt_fx_deps_cases:
