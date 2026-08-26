44324d27a38267baac4c08fc67ce88c04cb4674b
AMD <sasha.davidovich@10x-software.org>
2026-08-26 02:11:54 -0400

    1) simplified style_sheet.py 2) Changed README.md

---
+++ ui_10x/examples/constants.py
@@ -0,0 +1,16 @@
+from core_10x.named_constant import NamedConstant
+
+class COLOR(NamedConstant):
+    BLACK   = 'black'
+    WHITE   = 'white'
+    GREY    = 'grey'
+    RED     = 'red'
+    GREEN   = 'green'
+    LIGHTGREEN = 'lightgreen'
+    YELLOW  = 'yellow'
+    BLUE    = 'blue'
+
+class FONT(NamedConstant):
+    NEW_ROMAN   = 'Times New Roman'
+    HELVETICA   = 'Helvetica'
+    COURIER_NEW = 'Courier New'
